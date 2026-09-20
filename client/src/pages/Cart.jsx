d9292782ed0f6c6bcc7436aa6adcebea989852a4
Deepak <deepakrajpu500@gmail.com>
2026-09-20 14:14:18 +0530

    Fix Cart imports

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import {
     FaTrash, FaMinus, FaPlus, FaArrowLeft, FaArrowRight, FaShoppingBag, FaHeart, FaTruck, FaShieldAlt,
-    FaHeadset, FaInstagram, FaTwitter, FaFacebookF, FaTiktok, FaEnvelope, FaPhone
+    FaHeadset
 } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";

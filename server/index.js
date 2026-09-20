dd3d38f1e66a3006248b028a9a72551fb844e237
Deepak <deepakrajpu500@gmail.com>
2026-09-20 15:24:41 +0530

    Fix CORS middleware

--- server/index.js
+++ server/index.js
@@ -10,10 +10,10 @@ const Mongoose = require("./connection/connection");
 const router = require("./Router/Router");
 
 const app = express();
-cors({
+app.use(cors({
     origin: ['http://localhost:3000', 'https://fashion-hub-six-sable.vercel.app'],
     credentials: true
-})
+}));
 
 app.use(cookieParser());
 
